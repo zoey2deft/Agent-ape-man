764580b5c2a312f6311d539a4d31f2d7a3d3d388
joker <z694908768@gmail.com>
2026-08-14 23:20:31 +0800

    update env key lesson

--- lessons/01_read_env_key.py
+++ lessons/01_read_env_key.py
@@ -1,9 +1,11 @@
 """第 01 课：从环境变量读取 API Key，但绝不输出 Key 本身。"""
 
 import os
+from dotenv import load_dotenv
+load_dotenv()
 
 
-ENV_NAME = "OPENAI_API_KEY"
+ENV_NAME = "DEEPSEEK_API_KEY"
 api_key = os.getenv(ENV_NAME)
 
 if api_key:
